f76dac5c28f5d70a70f0d9689b92444e783432e5
Vasiliy Rozhkov <profilrv@gmail.com>
2024-05-17 15:40:48 +0500

    recursion function

--- 3-recursion/recursion.ts
+++ 3-recursion/recursion.ts
@@ -3,7 +3,24 @@ const fact = (value: number): number =>
         ? 1
         : value * fact(value - 1)
 
-
 console.log(fact(2))
 console.log(fact(3))
 console.log(fact(4))
+
+/**********************************************/
+
+const sum = (arr: number[]): number => {
+    if (arr.length === 0) {
+        return 0
+    } else {
+        let first = arr[0]
+        arr.splice(0, 1)
+
+        return first + sum(arr)
+    }
+}
+
+console.log(sum([0, 1, 2, 3]))
+console.log(sum([2, 33]))
+
+/**********************************************/
