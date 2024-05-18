67394cf1fc7232bb774aa0d410ce9dc8e055b89c
Vasiliy Rozhkov <profilrv@gmail.com>
2024-05-18 08:11:36 +0500

    recursion function

--- 3-recursion/recursion.ts
+++ 3-recursion/recursion.ts
@@ -1,14 +1,21 @@
+/**
+ * Factorial
+ * @param value
+ */
 const fact = (value: number): number =>
     value === 1
         ? 1
         : value * fact(value - 1)
 
-console.log(fact(2))
-console.log(fact(3))
-console.log(fact(4))
+console.log('Factorial', fact(2))
+console.log('Factorial', fact(3))
+console.log('Factorial', fact(4))
 
-/**********************************************/
 
+/**
+ * Sum of array's elements
+ * @param arr
+ */
 const sum = (arr: number[]): number => {
     if (arr.length === 0) {
         return 0
@@ -20,7 +27,50 @@ const sum = (arr: number[]): number => {
     }
 }
 
-console.log(sum([0, 1, 2, 3]))
-console.log(sum([2, 33]))
+console.log('Sum of array', sum([0, 1, 2, 3]))
+console.log('Sum of array', sum([2, 33]))
 
-/**********************************************/
+/**
+ * Sum of List's elements
+ */
+class ListNode {
+    next: ListNode | null
+    val: number
+
+    constructor(val: number, next: ListNode | null = null) {
+        this.next = next
+        this.val = val
+    }
+
+    set(val: number) {
+        this.val = val
+    }
+
+    getVal(): number {
+        return this.val
+    }
+
+    setNext(next: ListNode | null) {
+        this.next = next
+    }
+
+    getNext(): ListNode | null {
+        return this.next
+    }
+}
+
+let firstNode = new ListNode(3)
+let secondNode = new ListNode(5)
+firstNode.setNext(secondNode)
+let thirdNode = new ListNode(9)
+secondNode.setNext(thirdNode)
+
+function sumOfList(firstNode: ListNode | null): number {
+    if (firstNode === null) return 0
+
+    return firstNode.getNext() === null
+        ? firstNode.getVal()
+        : firstNode.getVal() + sumOfList(firstNode.getNext())
+}
+
+console.log('Sum of list', sumOfList(firstNode))
